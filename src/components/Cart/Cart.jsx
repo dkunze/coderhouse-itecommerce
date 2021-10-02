710279c9cd340cf7ae29cb5e78ef47ea0f48cdc5
dkunze <78369755+dkunze@users.noreply.github.com>
2021-10-02 19:12:52 -0300

    Remove unused vars

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useCartContext } from '../../Context/cartContext';
 import { Link } from 'react-router-dom';
 import { getFirestore } from '../../services/getFirebase';
